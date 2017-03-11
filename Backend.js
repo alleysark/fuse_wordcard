2fdcc2cd9933b1d57c7243c6d75a9b2a11004b14
alleysark <dev.alleysark@gmail.com>
2017-03-11 15:45:08 +0900

    split data handling from ui to backend script

--- Backend.js
+++ Backend.js
@@ -1,13 +1,71 @@
+var wordListDB = [
+        {
+            word: "alpha",
+            phoneticSign: "|ӕlfə",
+            example: "The top class is called Alpha-Plus",
+            meaning: "[NOUN] the first letter in the Greek alphabet (Α, α), a vowel transliterated as a",
+            lastViewTime: "2017-03-11T06:00:56.638Z",
+            lastKnowingTime: "2017-03-11T06:00:56.638Z",
+            knowingCount: 0
+        },
+        {
+            word: "beta",
+            phoneticSign: "|beɪtə",
+            example: "Thank you for trying out the BETA.",
+            meaning: "[NOUN] the second letter in the Greek alphabet (Β, β), a consonant, transliterated as b",
+            lastViewTime: "2017-03-11T06:00:56.638Z",
+            lastKnowingTime: "2017-03-11T06:00:56.638Z",
+            knowingCount: 0
+        },
+        {
+            word: "gamma",
+            phoneticSign: "|gӕmə",
+            example: "A study on the measurement of void fraction using gamma ray",
+            meaning: "[NOUN] Gamma is the third letter of the Greek alphabet.",
+            lastViewTime: "2017-03-11T06:00:56.638Z",
+            lastKnowingTime: "2017-03-11T06:00:56.638Z",
+            knowingCount: 0
+        }
+    ];
 
-function clone(obj) {
-	return JSON.parse(JSON.stringify(obj));
+function cloneJSONData(obj) {
+    return JSON.parse(JSON.stringify(obj));
 }
 
 function getMainWordList() {
-    console.log("not yet...");
-    return [];
+    
+    // TODO: some fantastic filtering
+
+    return cloneJSONData(wordListDB);
+}
+
+function updateWordView(word) {
+    wordListDB.every(function(item){
+        if (item.word !== word)
+            return true;
+        
+        date = new Date();
+        item.lastViewTime = date.toJSON();
+        return false;
+    });
+}
+
+function confirmWordKnowing(word, withoutShowMeaning) {
+    wordListDB.every(function(item){
+        if (item.word !== word)
+            return true;
+        
+        date = new Date();
+        item.lastKnowingTime = date.toJSON();
+        item.lastViewTime = date.toJSON();
+        if (withoutShowMeaning)
+            item.knowingCount = item.knowingCount + 1;
+        return false;
+    });
 }
 
 module.exports = {
-    getMainWordList: getMainWordList
+    getMainWordList: getMainWordList,
+    updateWordView: updateWordView,
+    confirmWordKnowing: confirmWordKnowing
 }
--- Pages/MainWordView/MainWordViewPage.js
+++ Pages/MainWordView/MainWordViewPage.js
@@ -1,33 +1,9 @@
 var Observable = require("FuseJS/Observable");
 var Backend = require("Backend.js");
 
-var wordListJsonData = [
-        {
-            word: "alpha",
-            phoneticSign: "|ӕlfə",
-            example: "The top class is called Alpha-Plus",
-            meaning: "[NOUN] the first letter in the Greek alphabet (Α, α), a vowel transliterated as a",
-            viewCount: 0,
-            lastViewTime: "170301-18:32"
-        },
-        {
-            word: "beta",
-            phoneticSign: "|beɪtə",
-            example: "Thank you for trying out the BETA.",
-            meaning: "[NOUN] the second letter in the Greek alphabet (Β, β), a consonant, transliterated as b",
-            viewCount: 0,
-            lastViewTime: "170301-18:32"
-        },
-        {
-            word: "gamma",
-            phoneticSign: "|gӕmə",
-            example: "A study on the measurement of void fraction using gamma ray",
-            meaning: "[NOUN] Gamma is the third letter of the Greek alphabet.",
-            viewCount: 0,
-            lastViewTime: "170301-18:32"
-        }
-    ];
-    
+
+var wordListJsonData = Backend.getMainWordList();
+
 wordListJsonData.forEach(function(item, idx){
     item.index = idx;
     item.isShowMeaning = Observable(false);
@@ -45,6 +21,10 @@ function onWordPageActivated(arg) {
 }
 
 function onConfirmKnowing(e) {
+    // confirm to database
+    Backend.confirmWordKnowing(e.data.word, !e.data.isShowMeaning);
+
+    // remove this item from visual word list
     wordList.remove(e.data);
     
     if (currentPage.value > 0) {
